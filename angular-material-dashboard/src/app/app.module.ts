477ffe1a187329bb9adc39f49b861e1cf543c36f
technologicaljerry <cserajatverma@gmail.com>
2022-12-02 16:31:48 +0530

    Implement material component

--- angular-material-dashboard/src/app/app.module.ts
+++ angular-material-dashboard/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -11,6 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    MatButtonModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
