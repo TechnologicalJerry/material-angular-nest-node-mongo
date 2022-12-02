d09b23728e94090a01811f502e5a7d9c93171fdd
technologicaljerry <cserajatverma@gmail.com>
2022-12-02 22:53:27 +0530

    Update

--- angular-material-dashboard/src/app/app.module.ts
+++ angular-material-dashboard/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     AppComponent
@@ -13,6 +13,7 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     MatButtonModule,
     AppRoutingModule,
+    MatToolbarModule,
     BrowserAnimationsModule
   ],
   providers: [],
