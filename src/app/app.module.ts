043d763832a8d0bda8a850e1059f7a654b5fa16b
technologicaljerry <cserajatverma@gmail.com>
2023-02-10 23:51:09 +0530

    Implement mat-button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    MatButtonModule,
     AppRoutingModule,
     MatSlideToggleModule,
     BrowserAnimationsModule
