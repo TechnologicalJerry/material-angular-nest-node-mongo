3bc1ad142394c3e3684dc49b762c458ad00a506b
technologicaljerry <cserajatverma@gmail.com>
2023-02-13 23:43:06 +0530

    Update material.module.ts

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MaterialRoutingModule } from './material-routing.module';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   imports: [
-    MaterialRoutingModule
+    MaterialRoutingModule,
+    MatButtonModule
   ],
   exports: []
 })
