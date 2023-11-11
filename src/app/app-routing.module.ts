6b2c286d6a1e9fb5cd47bbb4a94ccf6f51622e81
Rajat Verma <16414387+TechnologicalJerry@users.noreply.github.com>
2023-11-11 23:55:46 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathmatch: 'full' },
-  { path: '', component: LoginComponent, pathmatch: 'full' }
+  { path: '', component: LoginComponent, pathmatch: 'full' },
+  { path: '', component: SignupComponent, pathmatch: 'full' },
 ];
 
 @NgModule({
