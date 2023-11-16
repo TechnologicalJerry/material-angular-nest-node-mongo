c4254af2f6a5bf288f36111407ef0e4aced28c3e
technologicaljerry <cserajatverma@gmail.com>
2023-11-16 23:18:30 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
+// import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
