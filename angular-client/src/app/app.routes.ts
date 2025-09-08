d4f37d31369cd72e429e45ac88c7148603d39ddb
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-09 00:11:55 +0530

    Update app.routes.ts

--- angular-client/src/app/app.routes.ts
+++ angular-client/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { NotFound } from './shared/components/not-found/not-found';
+import { authGuard } from './core/guards/auth.guard';
+import { noAuthGuard } from './core/guards/no-auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./features/landing/landing/landing').then(m => m.Landing),
+        canActivate: [noAuthGuard]
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./features/auth/auth.routes').then(m => m.authRoutes),
+        canActivate: [noAuthGuard]
+    },
+    {
+        path: 'dashboard',
+        loadChildren: () => import('./features/dashboard/dashboard.routes').then(m => m.dashboardRoutes),
+        canActivate: [authGuard]
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('./features/about/about/about').then(m => m.About),
+    },
+    { path: '**', component: NotFound }
+];
