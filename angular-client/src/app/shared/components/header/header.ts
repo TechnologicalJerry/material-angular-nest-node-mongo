3ef55c7c113f6151d7bbbbc6ca6f67463f098493
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-09 00:12:40 +0530

    Update header.ts

--- angular-client/src/app/shared/components/header/header.ts
+++ angular-client/src/app/shared/components/header/header.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/services/auth';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
+  isLoggedIn$ = this.authService.user$;
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
