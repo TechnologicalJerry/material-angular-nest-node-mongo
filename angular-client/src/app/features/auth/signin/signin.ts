98039690c0b175d3b6061b81a38509cc1ab12aae
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-08 23:08:30 +0530

    Create signin.ts

---
+++ angular-client/src/app/features/auth/signin/signin.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-signin',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './signin.html',
+  styleUrl: './signin.scss'
+})
+export class Signin {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).pipe(
+        tap(() => this.router.navigate(['/dashboard']))
+      ).subscribe();
+    }
+  }
+}
