3c2c7dc0fb0ff0260042fc1ffa6c358bd4e50709
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-09 00:11:59 +0530

    Update signin.ts

--- angular-client/src/app/features/auth/signin/signin.ts
+++ angular-client/src/app/features/auth/signin/signin.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
-import { AuthService } from '../../../core/services/auth.service';
+import { AuthService } from '../../../core/services/auth';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs/operators';
 
