f30160327c3aac7e4b26df56a1a8947499c6049d
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-09 00:11:57 +0530

    Update auth.routes.ts

---
+++ angular-client/src/app/features/auth/auth.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { Signin } from './signin/signin';
+import { Signup } from './signup/signup';
+
+export const authRoutes: Routes = [
+    { path: 'signin', component: Signin },
+    { path: 'signup', component: Signup }
+];
