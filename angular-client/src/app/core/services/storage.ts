21a38b0148cd2536738ceccd0ce6e2003bce5bc4
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-10 11:30:48 +0530

    Update storage.ts

--- angular-client/src/app/core/services/storage.ts
+++ angular-client/src/app/core/services/storage.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { User } from '../models/user.model';
 
 @Injectable({
@@ -7,18 +8,29 @@ import { User } from '../models/user.model';
 export class StorageService {
   private readonly USER_KEY = 'currentUser';
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  private isBrowser(): boolean {
+    return isPlatformBrowser(this.platformId);
+  }
 
   saveUser(user: User): void {
-    localStorage.setItem(this.USER_KEY, JSON.stringify(user));
+    if (this.isBrowser()) {
+      localStorage.setItem(this.USER_KEY, JSON.stringify(user));
+    }
   }
 
   getUser(): User | null {
-    const user = localStorage.getItem(this.USER_KEY);
-    return user ? JSON.parse(user) : null;
+    if (this.isBrowser()) {
+      const user = localStorage.getItem(this.USER_KEY);
+      return user ? JSON.parse(user) : null;
+    }
+    return null;
   }
 
   removeUser(): void {
-    localStorage.removeItem(this.USER_KEY);
+    if (this.isBrowser()) {
+      localStorage.removeItem(this.USER_KEY);
+    }
   }
 }
