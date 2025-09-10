c4e386efc78c5a211fc177f395df35ff9a14a9ce
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-10 11:31:04 +0530

    Update dashboard.ts

--- angular-client/src/app/features/dashboard/dashboard/dashboard.ts
+++ angular-client/src/app/features/dashboard/dashboard/dashboard.ts
@@ -1,8 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Header } from '../../../shared/components/header/header';
+import { Footer } from '../../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    Header,
+    Footer,
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
