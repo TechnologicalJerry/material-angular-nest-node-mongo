043d6366c34e069aa6705429ce21bd9ca02b16da
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-10 11:30:43 +0530

    Update no-auth.guard.ts

--- angular-client/src/app/core/guards/no-auth.guard.ts
+++ angular-client/src/app/core/guards/no-auth.guard.ts
@@ -1,6 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../../core/services/auth';
 
 export const noAuthGuard: CanActivateFn = (route, state) => {
     const authService = inject(AuthService);
