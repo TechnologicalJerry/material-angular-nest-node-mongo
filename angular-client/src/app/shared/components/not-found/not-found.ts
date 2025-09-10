7089fee466eb4e9fb02e64597c265a12df8834fb
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-10 11:31:24 +0530

    Create not-found.ts

---
+++ angular-client/src/app/shared/components/not-found/not-found.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './not-found.html',
+  styleUrl: './not-found.scss'
+})
+export class NotFound {
+
+}
