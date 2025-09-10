d1fb52f68ff73bc530cd88b9e64f34b91dfa4f3a
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-10 11:30:55 +0530

    Update signin.ts

--- angular-client/src/app/features/auth/signin/signin.ts
+++ angular-client/src/app/features/auth/signin/signin.ts
@@ -1,6 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { AuthService } from '../../../core/services/auth';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs/operators';
@@ -8,7 +14,16 @@ import { tap } from 'rxjs/operators';
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule, 
+    RouterModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './signin.html',
   styleUrl: './signin.scss'
 })
@@ -16,6 +31,8 @@ export class Signin {
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
   private router = inject(Router);
+
+  hidePassword = true;
 
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
