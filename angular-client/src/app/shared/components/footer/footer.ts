5c31042848ba2bad30799488d4d412596cfab2b1
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-10 11:31:14 +0530

    Update footer.ts

--- angular-client/src/app/shared/components/footer/footer.ts
+++ angular-client/src/app/shared/components/footer/footer.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  standalone: true,
+  imports: [
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatCardModule,
+    MatListModule
+  ],
+  templateUrl: './footer.html'
 })
 export class Footer {
 
