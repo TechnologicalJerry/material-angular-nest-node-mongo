d1c9ced5892b0d3d6aaf6f7180435151a9aeb15d
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-10 11:30:35 +0530

    Update app.config.ts

--- angular-client/src/app/app.config.ts
+++ angular-client/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,6 +15,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(
+      withFetch(),
       withInterceptors([authInterceptor, errorInterceptor])
     ),
     importProvidersFrom(BrowserAnimationsModule)
