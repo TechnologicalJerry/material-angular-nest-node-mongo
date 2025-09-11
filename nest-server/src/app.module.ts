62b1340a545503aac512992db484ba90a9495762
TechnologicalJerry <cserajatverma@gmail.com>
2025-09-11 18:39:20 +0530

    Update app.module.ts

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,19 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { databaseConfig } from './config/database.config';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    MongooseModule.forRoot(databaseConfig.uri),
-    UsersModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
